78b441665e5fe7cfa554b5ea4a188e45ca389c9d
Yassine-cheffai <yacincheffai@yahoo.com>
2022-09-13 08:54:19 +0200

    comment unused import

--- src/components/resources/Pods.tsx
+++ src/components/resources/Pods.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { invoke } from '@tauri-apps/api';
-import { getPods } from "../../mocks";
+// import { getPods } from "../../mocks";
 import { Pod } from "../../types";
 
 
